c761468c6f99240b624229b10d1d61dcd5490b31
Adonis Darwish <adonis.darwish@gmail.com>
2021-04-26 18:20:01 +0400

    added fs and remotedialog

--- src/App.vue
+++ src/App.vue
@@ -27,30 +27,32 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
+      <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <index/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+import index from './components/index';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    index,
   },
 
   data: () => ({
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,8 +1,52 @@
 <template>
-    
+    <v-container>
+        <v-row class="text-center">
+            <v-col cols="12">
+                <v-btn @click="readDirectory">
+                    <span class="mr-2">Browse Folder</span>
+                </v-btn>
+                <h1>{{ chosenDir }} </h1>
+                <v-btn color="secondary" x-large @click="getFiles">
+                    <span class="mr-2">List Files</span>
+                </v-btn>
+                <ul>
+                    <li v-for="file in listOfFiles" :key="file">
+                        {{ file }}
+                    </li>
+                </ul>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
+const { remote } = require('electron');
+const fs = require('fs');
 export default {
-    
+    data: () => ({
+        chosenDir: '',
+        listOfFiles: [],
+    }),
+    methods: {
+        readDirectory() {
+            console.log("starting")
+            remote.dialog.showOpenDialog({
+            properties: ['openFile', 'openDirectory']
+            }).then(result => {
+                this.chosenDir = result.filePaths[0]
+                // this.getFiles()
+            // console.log(result.canceled)
+            console.log(result.filePaths)
+            }).catch(err => {
+            console.log(err)
+            });
+        },
+        getFiles() {
+            fs.readdir(this.chosenDir, (err, dir) => {
+            this.listOfFiles = dir;
+            // for(let filePath of dir)
+            //     console.log(filePath);
+            });
+        }
+    }
 }
 </script>
